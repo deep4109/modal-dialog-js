9b0f42664bdaeaeec4c393ef64cd250ce19d05b3
deep4109 <singhdeepankar41@gmail.com>
2024-11-16 22:05:26 +0530

    final commit

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const model = document.querySelector('.modal');
+console.log(model.classList);
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    model.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+btnClose.addEventListener('click', function () {
+  model.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+  model.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+document.addEventListener('keydown', function (e) {
+  console.log(e);
+  if (e.key === 'Escape') {
+    if (!model.classList.contains('hidden')) {
+      model.classList.add('hidden');
+      overlay.classList.add('hidden');
+    }
+  }
+});
